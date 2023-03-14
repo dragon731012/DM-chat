1081a793c943142a89f134c600e30f5caaf43ddb
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-03-14 15:02:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -344,6 +344,35 @@ function sendMessage() {
 		document.cookie="sox=true; expires=Thu, 18 Dec 9013 12:00:00 UTC"; 
 		window.location.reload(1);
 	  }
+	  let permission = Notification.permission;
+   if(permission === "granted"){
+      showNotification();
+   } else if(permission === "default"){
+      requestAndShowPermission();
+   } else {
+     alert("Use normal alert");
+   }
+   function requestAndShowPermission() {
+       Notification.requestPermission(function (permission) {
+           if (permission === "granted") {
+               showNotification();
+           }
+       });
+   }
+   function showNotification() {
+     //  if(document.visibilityState === "visible") {
+     //      return;
+     //  }
+      let title = "Chat";
+      let icon = 'https://homepages.cae.wisc.edu/~ece533/images/zelda.png'; //this is a large image may take more time to show notifiction, replace with small size icon
+      let body = value2;
+      let notification = new Notification(title, { body, icon });
+      notification.onclick = () => {
+             notification.close();
+             window.parent.focus();
+      }
+
+   }
 	  DOM.input.value = '';
 	  drone.publish({
 	    room: 'observable-room',
