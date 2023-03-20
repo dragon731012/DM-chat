4d13138e8c67ff273cc10338db3c1650b3df248a
JuliansABot <122099740+jsan3568@users.noreply.github.com>
2023-03-20 17:48:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const ownerlist=["Matteo","dragonmaster73101"];
 const adminlist=["Nate","Pax","Fedex Driver DragonJR","Bothacker1115","davey","dragonjr","Joey - Let's Ship out Your Order!"];
 const coadminlist=["Yoga"," king Andrew the ll","mason","KAISER WILHELM II"," C@MDEN","King cupcake lll"];
 const fedex=["testname"];
-const bot=["julianbotname"];
+const bot=["Bothacker1115"];
+const youreverydaydavey=["davey"]
 //PUT BANNED NAMES IN INDEX.HTML AND ADMIN NAMES IN CHANGEUSERNAME.HTML TOO
 function get_cookie(cookie_name) { const value = "; " + document.cookie; const parts = value.split("; " + cookie_name + "="); if (parts.length === 2) return parts.pop().split(";").shift(); }
 const drone = new ScaleDrone(CLIENT_ID, {
