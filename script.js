d45bb6a9c82a86f80839f5f79d992b5d394f67f3
DragonJR <128094238+dragonmasterjr2@users.noreply.github.com>
2023-03-20 17:15:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = 'm3BAnyjnCGqgWZye';
-const banlist=["paxton loves sex","barb","bean","p o r n_d a d d y","peyton","Gu@lp Gu@lp 3000","Queen cupcake","john","peebus"];
+const banlist=["paxton loves sex","barb","bean","p o r n_d a d d y","peyton","Gu@lp Gu@lp 3000","Queen cupcake","john","peebus","Typical g@m3^ M2s*n"];
 const ownerlist=["Matteo","dragonmaster73101"];
 const adminlist=["Nate","Pax","davey","julian","dragonjr","Fedex Driver DragonJR"];
 const coadminlist=["Yoga","mason"];
