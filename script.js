25da3086933c15f359c8adeee91adac8be531d49
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-03-20 17:11:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const ownerlist=["Matteo","dragonmaster73101"];
 const adminlist=["Nate","Pax","Fedex Driver DragonJR","julian","davey","dragonjr","Joey - Let's Ship out Your Order!","KAISER WILHELM II"," C@MDEN","King cupcake lll"];
 const coadminlist=["Yoga"," king Andrew the ll","mason"];
 const fedex=["testname"];
-const bot=["bothacker1115"]
-const youreverydaydavey=["davey"]
+const bot=["bothacker1115"];
+const youreverydaydavey=["davey"];
 //PUT BANNED NAMES IN INDEX.HTML AND ADMIN NAMES IN CHANGEUSERNAME.HTML TOO
 function get_cookie(cookie_name) { const value = "; " + document.cookie; const parts = value.split("; " + cookie_name + "="); if (parts.length === 2) return parts.pop().split(";").shift(); }
 const drone = new ScaleDrone(CLIENT_ID, {
