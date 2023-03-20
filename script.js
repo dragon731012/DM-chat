e15fdba2417db194f42298a9bbbe2130e3c45110
JuliansABot <122099740+jsan3568@users.noreply.github.com>
2023-03-20 18:13:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const CLIENT_ID = 'm3BAnyjnCGqgWZye';
 const ownerlist=["Matteo","dragonmaster73101"];
 const adminlist=["Nate","Pax","Fedex Driver DragonJR","Bothacker1115","davey","dragonjr","Joey - Let's Ship out Your Order!","davey2"];
-const coadminlist=["Yoga"," king Andrew the ll","mason","KAISER WILHELM II"," C@MDEN","King cupcake lll"];
+const coadminlist=["Yoga","andrew","mason","KAISER WILHELM II"," C@MDEN","King cupcake lll"];
 const fedex=["testname"];
 const bot=["Bothacker1115"];
-const andrew=["cupcake lord"];
+const andrew=["andrew"];
 const youreverydaydavey=["davey2"];
 //PUT BANNED NAMES IN INDEX.HTML AND ADMIN NAMES IN CHANGEUSERNAME.HTML TOO
 function get_cookie(cookie_name) { const value = "; " + document.cookie; const parts = value.split("; " + cookie_name + "="); if (parts.length === 2) return parts.pop().split(";").shift(); }
